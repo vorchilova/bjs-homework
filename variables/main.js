c3d644e79695e60e918d0324cd41afe38b28cc4e
Alina <vorchilova@inbox.ru>
2019-03-29 21:52:28 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,32 @@
+//задача 1
+let x, y, z, result;
+
+x = 2;
+y = 22;
+z = 0;
+
+result = x * y + 5 * z + x - 1;
+console.log('Результат ' + result);
+
+//задача 2
+let algebra;
+let geography;
+let physics;
+let total;
+
+algebra = 5;
+geography = 4;
+physics =5;
+
+total = (algebra + geography + physics) / 3;
+console.log('Итог ' + total);
+
+//задача 3
+
+let name, message;
+
+name = 'Alina';
+
+message = 'Привет, мир! Меня зовут ' + name;
+
+console.log(message);
