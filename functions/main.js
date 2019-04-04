b29355aa26afb7100393883c81ca85a2eac37539
Alina <vorchilova@inbox.ru>
2019-04-04 23:20:43 +0300

    first commit functions

---
+++ functions/main.js
@@ -0,0 +1,57 @@
+"use strict";
+let a, b, c;
+function getSolutions(a,b,c) {
+  let d = b*b - 4*a*c;
+  let x1, x2;
+    if (d < 0) {
+        return { D: d };
+    } else if (d === 0) {
+        x1 = -b / 2 * a;
+        return { D: d, roots: [x1] };
+    } else if (d > 0) {
+       x1 = (Math.sqrt(d) - b) / 2*a;
+       x2 = (-Math.sqrt(d) - b) / 2*a;
+       return { D: d, roots: [ x1, x2 ] };
+    }
+
+}
+
+function showSolutionsMessage(a,b,c) {
+  let result = getSolutions(a,b,c);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}\n Значение дискриминанта: ${result.D} \n `);
+  if (result.D < 0) {
+   console.log(`Уравнение не имеет вещественных корней`);
+} else if (result.D === 0) {
+   console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else if (result.D > 0) {
+    console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+    }
+}
+
+showSolutionsMessage(1,2,3);
+showSolutionsMessage(7,20,-3);
+showSolutionsMessage(2,4,2);
+
+
+function getPersonData( secretData ) {
+  function getName(num) {
+    let name;
+    if (num === 0) {
+      name = 'Родриго';
+    } else if (num === 1) {
+      name = 'Эмильо';
+    }
+    return name;
+  }
+ 
+  return {firstName: getName(secretData.aaa), lastName: getName(secretData.bbb)};
+}
+
+console.log(getPersonData({aaa: 1, bbb: 0}));
+console.log(getPersonData({aaa: 0, bbb: 0}));
+console.log(getPersonData({aaa: 0, bbb: 1}));
+console.log(getPersonData({aaa: 1, bbb: 1}));
+
+function getAverageScore( data ) {
+  
+}
